bba63df6cf0f1761066d3f99006b1dccd2b0c567
apoduval <apoduval@node0.profiling.scailmemorywall-pg0.wisc.cloudlab.us>
2025-04-23 15:45:32 -0500

    Add setup script. Add patch files for flexkvs and gapbs.

---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+git submodule init
+git submodule update
+
+sudo apt update
+sudo apt install libnuma-dev libpmem-dev libaio-dev libssl-dev
+
+# PEBS
+cd scripts/PEBS_page_tracking/
+git apply ../../pebs.patch
+make -j20
+cd ../..
+
+# flexkvs
+cd flexkvs
+git apply ../flexkvs.patch
+make -j20
+cd ..
+
+# GAPBS
+cd gapbs
+git apply ../gapbs.patch
+make bench-graphs -j20
+make -j20
+cd ..
+
+# graph_500
+cd graph_500
+git apply ../graph_500.patch
+cd src
+make -j20
+cd ../..
+
+# liblinear
+cd liblinear-2.47
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/kdd12.xz
+unxz kdd12.xz
+rm kdd12.xz
+make -j20
+cd ..
+
+# MERCI
+cd ../MERCI
+git apply ../merci.patch
+mkdir -p data/4_filtered/amazon_All
+cd data/4_filtered/amazon_All
+wget https://pages.cs.wisc.edu/~apoduval/MERCI/data/4_filtered/amazon_All/amazon_All_test_filtered.txt
+wget https://pages.cs.wisc.edu/~apoduval/MERCI/data/4_filtered/amazon_All/amazon_All_train_filtered.txt
+cd ../../..
+mkdir -p data/5_patoh/amazon_All/partition_2748/
+cd data/5_patoh/amazon_All/partition_2748/
+wget https://pages.cs.wisc.edu/~apoduval/MERCI/data/5_patoh/amazon_All/partition_2748/amazon_All_train_filtered.txt.part.2748
+cd ../../../..
+# now in merci
+cd 4_performance_evaluation/
+make -j20
+cd ../..
+# now in workloads
+
+# silo
+cd silo/silo
+git apply ../../silo.patch
+make dbtest -j20
+cd ../..
+
+# XSBench
+cd XSBench/openmp-threading
+make -j20
+cd ../..
