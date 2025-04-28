9b3dc2796e3adb37f495f5fb18f51b068ca7efaa
apoduval <apoduval@node0.profiling.scailmemorywall-pg0.wisc.cloudlab.us>
2025-04-27 21:46:29 -0500

    Update flexkvs data collection

--- scripts/workloads/flexkvs.sh
+++ scripts/workloads/flexkvs.sh
@@ -2,7 +2,7 @@
 
 config_flexkvs(){
     num_threads=8
-    kv_size=((32*1024*1024*1024))
+    kv_size=$((32*1024*1024*1024))
     warmup_time=20
     run_time=100
 }
@@ -16,7 +16,7 @@ run_flexkvs(){
 }
 
 run_strace_flexkvs(){
-    strace -e mmap,munmap -o xsbench_xsbench_strace.log $CUR_PATH/flexkvs/kvsbench -t $num_threads -T $run_time -w $warmup_time -h 0.25 127.0.0.1:1211 -S $kv_size
+    strace -e mmap,munmap -o flexkvs_flexkvs_strace.log $CUR_PATH/flexkvs/kvsbench -t $num_threads -T $run_time -w $warmup_time -h 0.25 127.0.0.1:1211 -S $kv_size
 }
 
 clean_flexkvs(){
