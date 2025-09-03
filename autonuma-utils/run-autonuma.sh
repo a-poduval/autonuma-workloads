190d8c654e2bcd27953ec338e7a0aaab2ea6c4bb
apoduval <apoduval@node0.autonuma-colloid.scailmemorywall-pg0.wisc.cloudlab.us>
2025-09-03 14:53:03 -0500

    Update autonuma run script to collect pcm data, edit uncore frequencies. Fix GAPBS bug where we were not setting threads. Add GUPS run command. Collect base and huge page usage information.

--- autonuma-utils/run-autonuma.sh
+++ autonuma-utils/run-autonuma.sh
@@ -11,7 +11,7 @@ fi
 
 # Set the application name and local tier size size
 APP=$1
-LSIZE=$2
+LSIZE=$2 # size of fast tier
 
 # NUMA Node
 NUM_THREADS=$3
@@ -39,13 +39,13 @@ mkdir -p "$LOG_DIR"
 
 # Take non-node 0 cores offline
 echo 0 | sudo tee /sys/devices/system/node/node1/cpu*/online >/dev/null 2>&1
-#echo 1 | sudo tee /sys/devices/system/node/node1/cpu39/online  # Keep one core online for uncore readings for pcm
-#numactl -C 39 bash cpu_burner.sh & # CPU Burner script to keep node 1 core occupied
+echo 1 | sudo tee /sys/devices/system/node/node1/cpu39/online  # Keep one core online for uncore readings for pcm
+numactl -C 39 bash cpu_burner.sh & # CPU Burner script to keep node 1 core occupied
 
 # Enable AutoNUMA balancing for tiered memory and configure local tier size
 echo 1 | sudo tee /sys/kernel/mm/numa/demotion_enabled
 echo 2 | sudo tee /proc/sys/kernel/numa_balancing
-#echo 1 | sudo tee /proc/sys/vm/zone_reclaim_mode
+echo 1 | sudo tee /proc/sys/vm/zone_reclaim_mode
 sudo ./node0_size_control.sh set $LSIZE
 
 # Drop the page cache to get consistent application performance measurements
@@ -57,7 +57,7 @@ if [[ "$APP" == gapbs_* ]]; then
     SUITE="gapbs"
     WORKLOAD="${APP#gapbs_}"
     #cd $HOME/gapbs
-    export OMP_NUM_THREADS=8
+    export OMP_NUM_THREADS=$NUM_THREADS
     RUN_CMD="$HOME/gapbs/${WORKLOAD} -f $HOME/gapbs/benchmark/graphs/${GRAPH_NAME}.sg -n 16"
     if [[ "$WORKLOAD" == bfs ]]; then
         RUN_CMD="$HOME/gapbs/${WORKLOAD} -f $HOME/gapbs/benchmark/graphs/${GRAPH_NAME}.sg -n 512"
@@ -83,6 +83,14 @@ elif [[ "$APP" == flexkvs ]]; then
     WORKLOAD="flexkvs"
     #cd $HOME/flexkvs
     RUN_CMD="$HOME/flexkvs/kvsbench -T 100 -w 20 -h 0.25 127.0.0.1:1211 -S 34359738368 -t $NUM_THREADS"
+elif [[ "$APP" == gups ]]; then
+    SUITE="gups"
+    WORKLOAD="gups"
+    # Reset size of fast tier, we are not using 4k pages for this workload
+    #sudo $HOME/autonuma-utils/node0_size_control.sh reset
+    #echo $LSIZE | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+    #echo 20000 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+    RUN_CMD="$HOME/gups_hemem/gups-hotset-move $NUM_THREADS 1000000000 35 8 33 n"
 elif [[ "$APP" == liblinear ]]; then
     SUITE="liblinear"
     #WORKLOAD="liblinear"
@@ -117,12 +125,12 @@ cat /proc/vmstat | grep "pgpromote\|pgdemote\|pgmigrate" >> "$LOG_DIR/$SUITE/${L
 #cat /proc/zoneinfo | grep "Node\|nr_\|workingset\|pgpromote\|pgdemote\|numa" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_procfs.txt"
 
 # Slow uncore frequencies
-#sudo wrmsr --processor 39 0x620 0x707
+sudo wrmsr --processor 39 0x620 0x707
 
 # Start Intel PCM in background and record in csv
 # Throws an error with cores offline, disabling for now
-#sudo pcm-memory $INTERVAL -csv="$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_pcm_memory.csv" &
-#PCM_MEM_PID=$!
+sudo pcm-memory $INTERVAL -csv="$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_pcm_memory.csv" &
+PCM_MEM_PID=$!
 
 # Capture performance counter data
 #$PERF_BIN stat -C 49-60 -I 2000 -e cycles,uops_retired.cycles,exe_activity.bound_on_loads,exe_activity.bound_on_stores,memory_activity.stalls_l1d_miss,memory_activity.stalls_l2_miss,memory_activity.stalls_l3_miss -o $LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_perf.csv -x, &
@@ -139,7 +147,7 @@ done
 
 # Monitor NUMA memory usage while workloads are running
 #echo "timestamp,node0_free_kb,node1_free_kb,node2_free_kb,node3_free_kb,node4_free_kb,node5_free_kb,node6_free_kb,node7_free_kb,node0_used_kb,node1_used_kb,node2_used_kb,node3_used_kb,node4_used_kb,node5_used_kb,node6_used_kb,node7_used_kb" > "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
-echo "timestamp,node0_free_kb,node1_free_kb,node0_used_kb,node1_used_kb" > "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
+echo "timestamp,node0_free_kb,node1_free_kb,node0_2M_free,node1_2M_free,node0_used_kb,node1_used_kb,node0_2M_total,node1_2M_total" > "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
 while true; do
     RUNNING=0
     for pid in "${PIDS[@]}"; do
@@ -156,26 +164,26 @@ while true; do
     TIMESTAMP=$(date +%s)
     NODE0=$(grep MemFree /sys/devices/system/node/node0/meminfo | awk '{print $4}')
     NODE1=$(grep MemFree /sys/devices/system/node/node1/meminfo | awk '{print $4}')
-    #NODE2=$(grep MemFree /sys/devices/system/node/node2/meminfo | awk '{print $4}')
-    #NODE3=$(grep MemFree /sys/devices/system/node/node3/meminfo | awk '{print $4}')
+    NODE2=$(grep HugePages_Free /sys/devices/system/node/node0/meminfo | awk '{print $4}')
+    NODE3=$(grep HugePages_Free /sys/devices/system/node/node1/meminfo | awk '{print $4}')
     #NODE4=$(grep MemFree /sys/devices/system/node/node4/meminfo | awk '{print $4}')
     #NODE5=$(grep MemFree /sys/devices/system/node/node5/meminfo | awk '{print $4}')
     #NODE6=$(grep MemFree /sys/devices/system/node/node6/meminfo | awk '{print $4}')
     #NODE7=$(grep MemFree /sys/devices/system/node/node7/meminfo | awk '{print $4}')
     #echo -n "$TIMESTAMP,$NODE0,$NODE1,$NODE2,$NODE3,$NODE4,$NODE5" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
     #echo -n "$TIMESTAMP,$NODE0,$NODE1,$NODE2,$NODE3,$NODE4,$NODE5,$NODE6,$NODE7" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
-    echo -n "$TIMESTAMP,$NODE0,$NODE1" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
+    echo -n "$TIMESTAMP,$NODE0,$NODE1,$NODE2,$NODE3" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
     NODE0=$(grep MemUsed /sys/devices/system/node/node0/meminfo | awk '{print $4}')
     NODE1=$(grep MemUsed /sys/devices/system/node/node1/meminfo | awk '{print $4}')
-    #NODE2=$(grep MemUsed /sys/devices/system/node/node2/meminfo | awk '{print $4}')
-    #NODE3=$(grep MemUsed /sys/devices/system/node/node3/meminfo | awk '{print $4}')
+    NODE2=$(grep HugePages_Total /sys/devices/system/node/node0/meminfo | awk '{print $4}')
+    NODE3=$(grep HugePages_Total /sys/devices/system/node/node1/meminfo | awk '{print $4}')
     #NODE4=$(grep MemUsed /sys/devices/system/node/node4/meminfo | awk '{print $4}')
     #NODE5=$(grep MemUsed /sys/devices/system/node/node5/meminfo | awk '{print $4}')
     #NODE6=$(grep MemUsed /sys/devices/system/node/node6/meminfo | awk '{print $4}')
     #NODE7=$(grep MemUsed /sys/devices/system/node/node7/meminfo | awk '{print $4}')
     #echo ",$NODE0,$NODE1,$NODE2,$NODE3,$NODE4,$NODE5" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
     #echo ",$NODE0,$NODE1,$NODE2,$NODE3,$NODE4,$NODE5,$NODE6,$NODE7" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
-    echo ",$NODE0,$NODE1" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
+    echo ",$NODE0,$NODE1,$NODE2,$NODE3" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_numa_meminfo.csv"
     sleep $INTERVAL
 done
 
@@ -187,7 +195,8 @@ done
 # Kill Perf
 #kill $PERF_PID
 # Kill PCM
-#kill $PCM_MEM_PID
+sudo kill $PCM_MEM_PID
+pkill -f pcm-memory
 
 # Dump pgpromote and demote stats at start
 echo "End" >> "$LOG_DIR/$SUITE/${LOG_NUMBER}_${NUM_THREADS}t_procfs.txt"
@@ -201,12 +210,16 @@ echo 0 | sudo tee /proc/sys/vm/zone_reclaim_mode
 sudo $HOME/autonuma-utils/node0_size_control.sh reset
 
 # Reset uncore frequencies
-#sudo wrmsr --processor 39 0x620 0xc14
+sudo wrmsr --processor 39 0x620 0xc14
 
 # Kill CPU burner script
 pkill -f cpu_burner.sh
 
 # Bring all cores online
 echo 1 | sudo tee /sys/devices/system/cpu/cpu*/online >/dev/null 2>&1
+
+# Reset huge pages
+echo 0 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+echo 0 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
 
 echo "Monitoring complete. Logs saved in $LOG_DIR"
